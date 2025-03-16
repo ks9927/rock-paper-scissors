bd86f40e1fb83195f03ca65d8923162174a93f9f
KS <180429671+ks9927@users.noreply.github.com>
2025-03-16 15:32:28 -0400

    Edited computer choice function
    Added a bunch of if/if else statements where if randomnum is a number 1 to 3 then return (and log) rock,paper,or scissors.
    I then called the function and now I get a random string each time i refresh the page.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function getComputerChoice() {
 	let randomNum = Math.floor(Math.random() *3) +1;
-    console.log(randomNum);
-	return randomNum; 
+
+    if(randomNum == 1) {
+        console.log("comp choice: rock");
+        return "rock";
+    }
+    else if (randomNum == 2) {
+        console.log("comp choice: paper");
+        return "paper";
+    }
+    else if (randomNum == 3) {
+        console.log("comp choice: scissors");
+        return "scissors";
+    }
+    else {
+        console.log("Error");
+    }
 	
 }
 
