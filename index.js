e0fe0743c8700e949520345c8384cf9121e17040
KS <180429671+ks9927@users.noreply.github.com>
2025-03-16 16:00:40 -0400

    Added comments to human choice and computer choice

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function getComputerChoice() {
+    // Gives a number between 1 and 3
 	let randomNum = Math.floor(Math.random() *3) +1;
 
+    // Checks if number is 1 to 3 and returns and logs a choice of rock, paper, or scissors
     if(randomNum == 1) {
         console.log("comp choice: rock");
         return "rock";
@@ -19,11 +21,16 @@ function getComputerChoice() {
 	
 }
 
+// Calling getComputerChoice function
 getComputerChoice();
 
 function getHumanChoice() {
+    //prompts user for an option of rock,paper,or scissors
 	let userChoice = prompt("Pick rock, paper, or scissors");
 
+    //if user doesn't enter anything then logs pls try again
+    //else it logs the user's entry
+    //When user presses ok then entry is returned automatically
 	if (userChoice === ""){
 	console.log("Please try again");
 	}
